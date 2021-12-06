cb03e16b941ca66459eb93ea829b6d6670d13e2f
rosemanjake <roseman.jake@gmail.com>
2021-12-06 20:12:50 +0000

    refactoring for simplicity

--- day6/day6.py
+++ day6/day6.py
@@ -1,40 +1,33 @@
 import os
+import copy
 
-class Incrementer:
-    def __init__(self, text, days):
-        self.state = Incrementer.get_state(text)
-        self.total = Incrementer.get_total(self.state, days)
-    
-    @staticmethod
-    def get_total(state, days):
-        for i in range(days):
-            state = Incrementer.increment(state)      
-        return sum(state.values())
+def get_state(text):
+    nums = [int(num) for num in text.split(",")]
+    counter = {}
+    for i in range(-1, 9):
+        counter[i] = 0
+        if i in nums:
+            counter[i] = nums.count(i)
+    return counter
 
-    @staticmethod
-    def increment(state):
-        counter = state
-        for num in state.keys():
-            if not counter[num] == 0 and not num == -1:
-                counter[num - 1] += counter[num]
-                counter[num] -= counter[num]
+def increment(state):
+    counter = state
+    for num in state.keys():
+        if not counter[num] == 0 and not num == -1:
+            counter[num - 1] += counter[num]
+            counter[num] -= counter[num]
 
-        if -1 in counter.keys():
-            counter[6] += counter[-1]
-            counter[8] += counter[-1]
-            counter[-1] = 0
-        return counter
+    if -1 in counter.keys():
+        counter[6] += counter[-1]
+        counter[8] += counter[-1]
+        counter[-1] = 0
+    return counter
 
-    @staticmethod
-    def get_state(text):
-        nums = [int(num) for num in text.split(",")]
-        counter = {}
-        for i in range(-1, 9):
-            counter[i] = 0
-            if i in nums:
-                counter[i] = nums.count(i)
-        return counter
+def get_total(state, days):
+    for i in range(days):
+        state = increment(state)      
+    return sum(state.values())
 
-text = open(f"{os.path.dirname(__file__)}\\day6.txt", "r").read()
-print(f"Number of fish after 80 days = {Incrementer(text, 80).total}")
-print(f"Number of fish after 80 days = {Incrementer(text, 256).total}")
+state = get_state(open(f"{os.path.dirname(__file__)}\\day6.txt", "r").read())
+print(f"Number of fish after 80 days = {get_total(copy.deepcopy(state), 80)}")
+print(f"Number of fish after 256 days = {get_total(state, 256)}")
