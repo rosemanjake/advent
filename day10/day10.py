3be019f22efa6da5be56aaafa5cacdf46fc386a0
rosemanjake <roseman.jake@gmail.com>
2021-12-10 09:11:58 +0000

    solved part 2

--- day10/day10.py
+++ day10/day10.py
@@ -1,4 +1,5 @@
 import os
+import statistics
 
 def get_input(file):
     f = open(f"{os.path.dirname(__file__)}\\{file}", "r")
@@ -14,16 +15,15 @@ def get_badchars(line):
                 return char
             else:
                 close = stack.pop()
+    return stack
 
-def part1(input):
-    lines = input.split("\n")
-
+def part1(lines):
     badchars = []
     for line in lines:
         badchars.append(get_badchars(line))
 
     total = 0
-    for char in [char for char in badchars if not None]:
+    for char in [char for char in badchars if not type(char) == list]:
         if char == ")":
             total = total + 3
         if char == "]":
@@ -35,6 +35,38 @@ def part1(input):
     
     return total
 
+def part2(lines):
+    incomplete = []
+    for line in lines:
+        incomplete.append(get_badchars(line))
+    
+    incomplete = [stack for stack in incomplete if type(stack) == list]
+
+    completes = []
+    for stack in incomplete:
+        complete = []
+        for char in stack:
+            complete.append(brackets[char])
+        complete.reverse()
+        completes.append(complete)
+
+    scoredict = {
+        ")":1,
+        "]":2,
+        "}":3,
+        ">":4,
+    }
+    
+    scores = []
+    for complete in completes:
+        score = 0
+        for char in complete:
+            score = score * 5
+            score = score + scoredict[char]
+        scores.append(score)
+
+    return statistics.median(scores)
+
 brackets = {
     "(":")",
     "[":"]",
@@ -43,5 +75,6 @@ brackets = {
 }
 
 input = get_input("day10.txt")
-score = part1(input)
-print(score)
+lines = input.split("\n")
+print(f"Solution for part 1 = {part1(lines)}")
+print(f"Solution for part 2 = {part2(lines)}")
